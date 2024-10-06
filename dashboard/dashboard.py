8ee39d61358265ff3635237b5788705350fc167c
ClaysHere <muhammadfikri4402@gmail.com>
2024-10-06 10:56:44 +0700

    fix

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,7 +20,7 @@ max_date = all_df["dteday"].max()
 
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image("./bikes.jpg")
+    st.image("https://raw.githubusercontent.com/ClaysHere/project_bike_sharing/refs/heads/main/dashboard/bikes.jpg")
     
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
